f68d1e79110a4e8cd069dacf1d58ad65056f5535
dteso <dtesodev@gmail.com>
2021-06-26 12:46:23 +0200

    proyecto inicial

---
+++ README.md
@@ -0,0 +1,129 @@
+# BLUETOOTH SERIAL TERMINAL
+
+## Usar este proyecto
+
+Recuerda tener Android Studio instalado para generar tus apk.
+
+1. Clónate este proyecto en un directorio local, accede a una consola, navega en ella hasta la ruta donde
+has clonado el proyecto y ejecuta el comando
+
+--> npm install
+
+2. A continuación ejecuta:
+
+ionic cap add android
+
+ionic cap build android
+
+ionic cap sync
+
+3. En este punto recuerda que el equipo desde donde lanzas el servidor y el dispositivo en el que estás volcando el programa deben estar conectados a la misma red y a través de cable USB. Una vez verificado 
+
+ionic cap run android -l --external 
+
+o instala la aplicación en tu dispositivo mediante Android Studio.
+
+
+*** NOTAS ***
+
+En el momento de desarrollar la aplicación, se está realizando sobre:
+
+- una versión Gradle 7.0 
+- Android Studio se encuentra en su versión 4.2 
+
+Actualización descargada el día 23/03/2021.
+
+
+
+## Pasos seguidos durante la generación de este proyecto
+
+1. Instalación global de cli
+
+npm install -g @ionic/cli native-run cordova-res
+
+2. Generación de aplicación Ionic con Capacitor como herramienta inicial para el framework Angular
+
+ionic start bluetoothterminal blank --type=angular --capacitor
+
+
+* IONIC INFO
+
+En el momento de estasconfiguraciones iniciales el proyecto arranca a fecha 24/06/2021 con las siguientes versiones:
+
+Ionic Info - 24/06/2021 
+Ionic:
+
+   Ionic CLI                     : 6.16.3 (C:\Users\d_tes\AppData\Roaming\npm\node_modules\@ionic\cli)
+   Ionic Framework               : @ionic/angular 5.6.10
+   @angular-devkit/build-angular : 12.0.5
+   @angular-devkit/schematics    : 12.0.5
+   @angular/cli                  : 12.0.5
+   @ionic/angular-toolkit        : 4.0.0
+
+Capacitor:
+
+   Capacitor CLI      : 3.0.2
+   @capacitor/android : 3.0.2
+   @capacitor/core    : 3.0.2
+   @capacitor/ios     : not installed
+
+Utility:
+
+   cordova-res : 0.15.3
+   native-run  : 1.4.0
+
+System:
+
+   NodeJS : v14.17.1 (C:\Program Files\nodejs\node.exe)
+   npm    : 6.14.13
+   OS     : Windows 10
+
+Crear aplicación: ionic start [blank | tabs | sidemenu] --type=[angular | react | vue] --capacitor?
+
+Arrancar server local: ionic serve ---> localhost:8100
+
+
+* Capacitor:
+
+   ionic cap sync ----> ionic capacitor sync [<platform>] [options] => Realiza lo siguiente:
+    - Genera una build para Ionic que compila los web assets
+    - Copia los web assets a las plataformas nativas de Capacitor correspondientes
+    - Actualiza e instala los plugins y dependencias correspondientes
+
+   [ionic cap add android] ---> ionic capacitor add <platform> [options] => Agrega un directorio 'platform' con el nombre de la plazaforma donde se incluiran los archivos para la construcción del apk
+
+   [ionic build / ionic cap build android] ---> ionic capacitor build <platform> [options] => Hace las siguientes tareas:
+
+    - Genera una build web
+    - Copia los archivos estáticos en el directorio correspondiente a la plataforma indicada (Ej: nombre_proyecto/android).
+    - Abre el IDE correspondiente a la plataforma del proyecto ( XCode o Android )
+
+   [ionic cap open] ---> onic capacitor open <platform> [options] => Abre el id cargando la última build generada
+
+
+> Livereloading:
+
+    - Sobre un dispositivo móvil, debes estar conectado a la misma red local que el equipo donde se ha arrancado la aplicación.
+    En caso contrario, se mostrará la pantalla en blanco finalizando con un timeout error.
+
+                                    ionic cap run android -l --external
+
+
+
+* Instalación plugin Capacitor-BLE-Central
+
+    npm i @ionic-native/core
+    npm install cordova-plugin-bluetooth-serial
+    npm install @ionic-native/bluetooth-serial
+    ionic cap sync
+
+   Si ejecutamos directamente en este punto en nuestro dispositivo,
+   en consola veremos que tenemos un error de permisos de localización 'Location'.
+   Vamos a resolverlo gestionando los permisos a la entrada de la aplicación mediante
+   el plugin de capacitor: 
+
+    npm install cordova-plugin-android-permissions
+    npm install @ionic-native/android-permissions 
+
+
+
---
+++ src/app/home/home.page.ts
@@ -0,0 +1,197 @@
+/* eslint-disable @typescript-eslint/prefer-for-of */
+/* eslint-disable no-console */
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+import { Observable, of } from 'rxjs';
+import { LoaderService } from 'src/services/loader.service';
+
+const READ_STATUS = '>>>READ_STATUS';
+
+interface DataRow {
+  id: number;
+  content: string;
+  type: string; // sent / received
+  timestamp: Date;
+}
+
+const MessageTypes = {
+    SENT: "SENT",
+    RECEIVED: "RECEIVED"
+}
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomePage implements OnInit {
+  bluetoothDevices = [];
+  rawData: DataRow[] = [];
+  rawDataSubject = new Observable<any>();
+  str = '';
+  lastStr = '';
+  currentStr = '';
+  codeInput = '';
+  connectionMessage = READ_STATUS;
+  idx: number;
+  messageTypes = MessageTypes;
+
+  connected = false;
+
+  btForm: FormGroup;
+
+  currentDevice = {
+    name: '',
+    address: '',
+  };
+
+  constructor(
+    private readonly bt: BluetoothSerial,
+    private readonly androidPermissions: AndroidPermissions,
+    private readonly cdr: ChangeDetectorRef,
+    private readonly formBuilder: FormBuilder,
+    public loaderService: LoaderService
+  ) {
+    this.idx=0;
+    this.btForm = this.formBuilder.group({
+      message: ['', Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.androidPermissions
+      .requestPermissions([
+        this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION,
+      ])
+      .then(() => {
+        this.androidPermissions
+          .checkPermission(
+            this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION
+          )
+          .then(
+            (result) => {
+              console.log('Has permission?', result.hasPermission);
+              this.bt.isConnected().then((connected)=>{ this.bt.disconnect()})
+              this.getStoredDevices();
+            },
+            (err) =>
+              this.androidPermissions.requestPermission(
+                this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION
+              )
+          );
+      });
+  }
+
+  onSubmit() {
+    if (!this.btForm.invalid) {
+      this.idx++;
+      this.rawData.push({
+        id: this.idx,
+        content: this.btForm.controls.message.value,
+        type: MessageTypes.SENT,
+        timestamp: new Date()
+      });
+      this.bt.write(this.btForm.controls.message.value);
+      this.btForm.controls.message.reset();
+    }
+  }
+
+
+  getStoredDevices() {
+    this.bt.list().then((devs) => {
+      console.log(JSON.stringify(devs));
+      this.bluetoothDevices.push(devs);
+      this.cdr.detectChanges();
+    });
+  }
+
+  getNow() {
+    return new Date();
+  }
+
+  openNativeBluettothSettings() {
+    this.bt.showBluetoothSettings();
+  }
+
+  connectToDevice(name: string, mac: string, messageOnConnection?) {
+    this.loaderService.presentLoading(`Conectando con ${name}`);
+    this.bt.connect(mac).subscribe((connected) => {
+      console.info('bluetooth connection: ', connected);
+      if (connected === 'OK') {
+        this.loaderService.hideLoading();
+        this.connected = true;
+        this.currentDevice = { name, address: mac };
+        this.cdr.detectChanges();
+      }
+      if (messageOnConnection) {
+        this.bt.write(messageOnConnection);
+      }
+      this.bt.subscribeRawData().subscribe((res) => {
+        const view = new Uint8Array(res);
+        if (view.length >= 1) {
+          for (let i = 0; i < view.length; i++) {
+            //if we received a \n, the message is complete, display it
+            if (view[i] == 13 || view[i] == 0) {
+              this.lastStr = this.str;
+              this.str = '';
+              this.cdr.detectChanges();
+            }
+            // if not, concatenate with the begening of the message
+            else {
+              let temp_str = String.fromCharCode(view[i]);
+              let str_esc = escape(temp_str);
+              this.codeInput = unescape(str_esc);
+              this.str += unescape(str_esc);
+            }
+          }
+        }
+        console.log(this.lastStr);
+        if (this.currentStr !== this.lastStr && this.lastStr !== '') {
+          this.idx++;
+          this.rawData.push({
+            id: this.idx,
+            content: this.lastStr,
+            type: MessageTypes.RECEIVED,
+            timestamp: new Date()
+          });
+          this.currentStr = this.lastStr;
+        } else {
+          this.lastStr = '';
+        }
+        this.rawDataSubject = of(this.rawData);
+        this.cdr.detectChanges();
+      });
+    }, (err)=>{
+      this.loaderService.hideLoading();
+      console.error("Conection Lost... ");
+    });
+
+    this.rawData.push({
+      id: this.idx,
+      content: this.lastStr,
+      type: MessageTypes.RECEIVED,
+      timestamp: new Date()
+    });
+  }
+
+  disconnect(){
+    this.bt.disconnect().then(res => {
+      console.info("Disconnected... " + res );
+      this.connected=false;
+      this.currentDevice=null;
+      this.lastStr = null;
+      this.rawData = [];
+      this.rawDataSubject = null;
+      this.getStoredDevices();
+    }).catch( err => console.error("Unable to disconnect"))
+  }
+
+}
